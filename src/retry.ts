ad89f05512b8309c54c453fb19159784d2895493
zzacal <jizacal@gmail.com>
2022-04-13 22:49:26 -0700

    Added documentation

--- src/retry.ts
+++ src/retry.ts
@@ -1,7 +1,13 @@
+/**
+ * Calls each function until success.
+ * @param {Array<() => T>} funcs - tried in order from [0]
+ * @param {(result: T) => boolean}  success - returns true if result is successful (default returns false)
+ * @param {number} tries - for each function (default 1)
+ */
 export const retry = <T>(
   funcs: Array<() => T>,
   success: (result: T) => boolean = () => false,
-  tries = 1
+  tries: number = 1
 ) => {
   funcs.reduce((result: boolean, current: () => T) => {
     if (result == true) {
